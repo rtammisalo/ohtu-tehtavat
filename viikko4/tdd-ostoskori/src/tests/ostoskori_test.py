785dd8796263c6588ea98c5790d25e17cfd55cfd
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-11-28 22:14:38 +0200

    Lisää 2 eri tuotteen lisäyksen ostoslistan pituuden testaukselle (TDD osa 10)

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -69,3 +69,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.lukumaara(), 1)
+
+    # step 10
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_kaksi_ostosoliota(self):
+        maito = Tuote("Maito", 3)
+        suklaa = Tuote("Suklaa", 2)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(suklaa)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
