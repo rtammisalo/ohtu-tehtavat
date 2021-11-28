fcdef49a351f8826b7ff074d21721ef0680d647d
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-11-28 22:20:11 +0200

    Lisää 2 saman tuotteen lisäyksen ostoslistan sisällön testaukselle (TDD osa 12)

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -86,5 +86,12 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(maito)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 1)
+
+    # step 12
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_ostoksen_jolla_on_sama_nimi_ja_lkm(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
         self.assertEqual(ostokset[0].lukumaara(), 2)
         self.assertEqual(ostokset[0].tuotteen_nimi(), "Maito")
