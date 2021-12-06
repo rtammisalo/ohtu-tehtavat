201b531fdc673f21ce39ddd152ff2ba9dfc937d5
Roni Tammisalo <81182631+rtammisalo@users.noreply.github.com>
2021-12-06 17:22:08 +0200

    All, Not ja HasFewerThan -luokkien toteutukset

--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -1,13 +1,14 @@
 class And:
     def __init__(self, *matchers):
         self._matchers = matchers
-    
+
     def matches(self, player):
         for matcher in self._matchers:
             if not matcher.matches(player):
                 return False
-        
+
         return True
+
 
 class PlaysIn:
     def __init__(self, team):
@@ -15,6 +16,7 @@ class PlaysIn:
 
     def matches(self, player):
         return player.team == self._team
+
 
 class HasAtLeast:
     def __init__(self, value, attr):
@@ -25,3 +27,31 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+
+class All:
+    def matches(self, player):
+        return True
+
+
+class Not:
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def matches(self, player):
+        for matcher in self._matchers:
+            if matcher.matches(player):
+                return False
+
+        return True
+
+
+class HasFewerThan:
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def matches(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
